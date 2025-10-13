d0471560be6ee2633b131f5efa37f61c6707991d
swirvtheman <swirvtheman@gmail.com>
2025-10-13 13:52:38 +0200

    Updated to include common files to igore.

---
+++ src/main/java/dev/gruppuppgift/Main.java
@@ -0,0 +1,6 @@
+package dev.gruppuppgift;
+public class Main {
+    public static void main(String[] args) {
+        new Program().run();
+    }
+}
---
+++ src/test/java/dev/gruppuppgift/ProgramTest.java
@@ -0,0 +1,25 @@
+package dev.gruppuppgift;
+
+import org.junit.Test;
+
+public class ProgramTest {
+    @Test
+    public void testRun() {
+
+    }
+
+    @Test
+    public void extendTime() {
+        
+    }
+
+    @Test
+    public void returnBook() {
+        
+    }
+
+    @Test
+    public void borrowBook() {
+        
+    }
+}
