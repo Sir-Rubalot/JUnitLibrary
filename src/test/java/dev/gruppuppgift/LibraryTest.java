2d8fd7f97f0c6f6b968b0346ccc069b0766c6414
Sir-Rubalot <robinekbladh@hotmail.com>
2025-10-16 18:54:57 +0200

    Test listAvailibleBooks

--- src/test/java/dev/gruppuppgift/LibraryTest.java
+++ src/test/java/dev/gruppuppgift/LibraryTest.java
@@ -1,24 +1,43 @@
 package dev.gruppuppgift;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.Test;
 
 public class LibraryTest {
     @Test
     public void testAdvanceDay() {
-
+        Book book = new Book("Title", "Genre", "Author");
+        assertEquals(0, book.getDaysBorrowed());
+        book.advanceDay();
+        assertEquals(1, book.getDaysBorrowed());
+        book.advanceDay();
+        assertEquals(2, book.getDaysBorrowed());
     }
-
-        // Suggestion: Test to see if the book's been borrowed already
-        // Suggestion: Test to see if the book's in stock
     
     @Test
     public void testExtendTime() {
-
+        Book book = new Book("testTitle", "testTenre", "testAuthor");
+        book.setDaysBorrowed(7);
+        assertEquals(7, book.getDaysBorrowed());
+        book.extendTime();
+        assertEquals(0, book.getDaysBorrowed());
     }
 
     @Test
     public void testListAvailableBooks() {
-
+        Library library = new Library();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+        library.listAvailableBooks();
+        String output = outputStream.toString();
+        assertTrue("Output should contain 'Harry Potter'", output.contains("Harry Potter"));
+        assertTrue("Output should contain 'ondskan'", output.contains("Ondskan"));
+        System.setOut(System.out);
     }
 
     @Test
